8a189f523958af30170e6478ff49f8e6c12c21a4
oldGreg <oldGreg@laptop-y580>
2016-09-27 20:41:54 +0200

    fix

--- esp_flash_size_check/esp_flash_size_check.ino
+++ esp_flash_size_check/esp_flash_size_check.ino
@@ -4,12 +4,38 @@
  This sketch tests if the EEPROM settings of the IDE match to the Hardware
 
  */
+#include <ArduinoOTA.h>
+#include <ESP8266WiFi.h>
+
+const char *ssid = "";        // cannot be longer than 32 characters!
+const char *pass = "";       // WiFi password
 
 void setup(void) {
 	Serial.begin(115200);
+	WiFi.mode(WIFI_STA);
+
+	ArduinoOTA.begin();
 }
 
 void loop() {
+	if (WiFi.status() != WL_CONNECTED) {
+		Serial.print("Connecting via WiFi to ");
+		Serial.print(ssid);
+		Serial.println("...");
+
+		WiFi.begin(ssid, pass);
+
+		if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+			return;
+		}
+
+		Serial.println("");
+		Serial.println("WiFi connect: Success");
+		Serial.print("IP address: ");
+		Serial.println(WiFi.localIP());
+
+	}
+	ArduinoOTA.handle();
 
 	uint32_t realSize = ESP.getFlashChipRealSize();
 	uint32_t ideSize = ESP.getFlashChipSize();
